7e152019db4fb00ec46d54af5d15dbf45f2a3273
Yunus Pagel <pagel.yunus@gmail.com>
2025-06-07 21:57:06 +0200

    feat: declare in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+
 #include "board.h"
+#include "snake.h"
 
 
 int main(){
 
     Board board(40, 20);
+    Snake snake({5, 5});
+
     board.draw();
+
+    for (int i = 0; i < 5; ++i) {
+        snake.draw();
+        snake.move();
+        std::this_thread::sleep_for(std::chrono::milliseconds(500)); // 0,5 Sek Pause
+        std::cout << "-----\n";
+    }
+
 
     return 0;
 }
